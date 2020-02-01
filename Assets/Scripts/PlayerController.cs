8992ab2199633db4278eb717b322195f450d0e03
Shkire <danielsmash94@hotmail.com>
2020-02-01 02:58:37 +0000

    Basic PlayerController added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 1f;
+    private Rigidbody2D _rigidBody;
+    private float _horizontalValue;
+
+    private void OnEnable()
+    {
+        if (_rigidBody == null)
+        {
+            _rigidBody = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void Update()
+    {
+        _horizontalValue = Input.GetAxis("Horizontal");
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 aux = _rigidBody.velocity;
+        aux.x = _horizontalValue * Speed;
+        _rigidBody.velocity = aux;
+    }
+}
