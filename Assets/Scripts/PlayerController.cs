b1cad313fdbc103174efb4a0dfde0ca444e10f58
Shkire <danielsmash94@hotmail.com>
2020-02-01 03:45:01 +0000

    Basic Stairs behaviour

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class PlayerController : MonoBehaviour
     private float Speed = 1f;
     private Rigidbody2D _rigidBody;
     private float _horizontalValue;
+    private float _verticalValue;
+    private bool _onStair;
+    private bool _usingStairs;
 
     private void OnEnable()
     {
@@ -21,14 +25,47 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _horizontalValue = Input.GetAxis("Horizontal");
-
-
+        _verticalValue = Input.GetAxis("Vertical");
     }
 
     private void FixedUpdate()
     {
+        if (_onStair && !_usingStairs)
+        {
+            if (_verticalValue != 0)
+            {
+                _usingStairs = true;
+            }
+        }
+
+
+        if (_usingStairs)
+        {
+            _rigidBody.isKinematic = true;
+            _rigidBody.velocity = new Vector2(0f, _verticalValue);
+            return;
+        }
+
         Vector3 aux = _rigidBody.velocity;
         aux.x = _horizontalValue * Speed;
         _rigidBody.velocity = aux;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Stair")
+        {
+            _onStair = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Stair")
+        {
+            _onStair = false;
+            _usingStairs = false;
+            _rigidBody.isKinematic = false;
+        }
     }
 }
