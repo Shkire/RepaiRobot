450bc12c63956006040719b08971dba8bd991808
Shkire <danielsmash94@hotmail.com>
2020-02-02 01:04:07 +0000

    BasicFireZone

---
+++ Assets/Scripts/ExtinguiserConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtinguiserConfig : MonoBehaviour
+{
+    public AnimationCurve _scaleCurve;
+    public AnimationCurve _distanceCurve;
+    public Transform parent;
+}
---
+++ Assets/Scripts/FireZone.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireZone : MonoBehaviour
+{
+    public float value;
+
+    private List<Collider2D> _colliders;
+    private SpriteRenderer _spriteRenderer;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (_colliders == null)
+        {
+            _colliders = new List<Collider2D>();
+        }
+
+        if (!_colliders.Contains(collision))
+        {
+            _colliders.Add(collision);
+        }
+    }
+
+    private void Update()
+    {
+        if (_colliders != null)
+        {
+            foreach (Collider2D collider in _colliders)
+            {
+                ExtinguiserConfig config = collider.GetComponent<ExtinguiserConfig>();
+                if (config != null)
+                {
+                    value -= Time.deltaTime * config._scaleCurve.Evaluate(new Vector2(config.parent.localScale.x, config.parent.localScale.y).magnitude) * config._distanceCurve.Evaluate(Vector2.Distance(transform.position, collider.transform.position));
+                }
+            }
+            _colliders.Clear();
+        }
+
+        if (value <= 0)
+        {
+            enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = Color.red;
+    }
+
+    private void OnDisable()
+    {
+        _spriteRenderer.color = Color.white;
+    }
+}
